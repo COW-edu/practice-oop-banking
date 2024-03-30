db93f6ff141b15575e437b1d9941d92c4dbe958b
jinhyeon <tiemo0708@naver.com>
2024-03-30 23:38:11 +0900

    test: 예금계좌가 실제로 저장되었는지 확인하는 테스트

---
+++ src/test/java/member/DepositMemberServiceTest.java
@@ -0,0 +1,29 @@
+package member;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositMemberServiceTest {
+    DepositMemberService depositMemberService;
+    @BeforeEach()
+    public void beforeEach(){
+        MemberAppconfig memberAppconfig = new MemberAppconfig();
+        depositMemberService = memberAppconfig.depositMemberService();
+    }
+    @Test
+    //계좌생성확인
+    void join() {
+        BigDecimal amount = new BigDecimal("10000");
+        //given
+        DepositMember depositMember = new DepositMember("N","NAME","12345",amount,true);
+        //when
+        depositMemberService.join(depositMember);
+        DepositMember getAccountInfo = depositMemberService.getAccountInfo("12345");
+        //then
+        assertEquals(depositMember,getAccountInfo);
+    }
+}
