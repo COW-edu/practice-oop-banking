3752678dae398fc1be864091471eca35f3237b83
jinhyeon <tiemo0708@naver.com>
2024-04-01 19:22:11 +0900

    feat: 송금, 계좌생성 완료메세지

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class OutputView {
     private static final String WITHDRAW_MESSAGE = "%s 계좌에서 %s원 출금이 완료되었습니다.%n";
     private static final String DEPOSIT_MESSAGE = "%s 계좌에 %s원 입금이 완료되었습니다.%n";
+    private static final String CREATE_ACCOUNT_MESSAGE = "%s 님 계좌생성이 완료되었습니다.%n" ;
+    private static final String TRANSFER_MESSAGE = "%s 계좌에서 $계좌로 %s원 송금이 완료되었습니다.%n";
     public static void withdrawEndMessage(String accountNumber, String amount) {
         System.out.printf(WITHDRAW_MESSAGE, accountNumber, amount);
     }
@@ -13,6 +15,15 @@ public class OutputView {
     public static void depositMessage(String accountNumber, String amount) {
         System.out.printf(DEPOSIT_MESSAGE, accountNumber, amount);
     }
+
+    public static void createAccountEnd(String name) {
+        System.out.printf(CREATE_ACCOUNT_MESSAGE,name);
+    }
+
+    public static void transferEnd(String fromAccount, String toAccount, String amount) {
+        System.out.printf(TRANSFER_MESSAGE,fromAccount,toAccount,amount);
+    }
+
 
     public void printAccountInfo(List<String> combinedInfo) {
         combinedInfo.forEach(System.out::println);
