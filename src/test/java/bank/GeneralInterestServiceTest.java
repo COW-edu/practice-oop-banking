d4d41b221f5c74ea035ee6402c44762c3c9e3896
jinhyeon <tiemo0708@naver.com>
2024-04-01 18:06:36 +0900

    test: 출금, 이자률확인 테스트

--- src/test/java/bank/GeneralInterestServiceTest.java
+++ src/test/java/bank/GeneralInterestServiceTest.java
@@ -1,5 +1,6 @@
 package bank;
 
+import interest.InterestService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import util.Appconfig;
@@ -10,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GeneralInterestServiceTest {
     BankService bankService;
+    InterestService interestService;
     @BeforeEach()
     public void beforeEach(){
         Appconfig appconfig = new Appconfig();
         bankService = appconfig.bankService();
+        interestService=appconfig.interBankService();
     }
     @Test
     //계좌생성확인
@@ -41,5 +44,35 @@ class GeneralInterestServiceTest {
         //then
         BigDecimal answerValue = new BigDecimal("20000");
         assertEquals(answerValue,getAccountInfo.getAmount());
+    }
+
+    @Test
+    //출금확인
+    void withdraw() {
+        //given
+        BigDecimal amount = new BigDecimal("10000");
+        BigDecimal witdrawAmount = new BigDecimal("5000");
+        GeneralMember generalMember = new GeneralMember("N","NAME","12345",amount,true);
+        //when
+        bankService.join(generalMember);
+        bankService.withdraw("12345",witdrawAmount);
+        GeneralMember getAccountInfo = bankService.getAccountInfo("12345");
+        //then
+        BigDecimal answerValue = new BigDecimal("5000");
+        assertEquals(answerValue,getAccountInfo.getAmount());
+    }
+
+    @Test
+    //이자률 확인
+    void interest() {
+        BigDecimal amount = new BigDecimal("10000");
+        BigDecimal depositAmount = new BigDecimal("10000");
+        GeneralMember generalMember = new GeneralMember("N","NAME","12345",amount,true);
+        //when
+        bankService.join(generalMember);
+
+        //then
+        BigDecimal answerValue = new BigDecimal("10200.00");
+        assertEquals(answerValue,interestService.getInterestEstimated("12345"));
     }
 }
