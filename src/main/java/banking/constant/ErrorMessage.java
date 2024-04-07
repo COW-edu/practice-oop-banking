6674078c685542630016c28d7612e4a5350f01bf
jaehyuk <bjh010321@naver.com>
2024-04-08 01:24:50 +0900

    :sparkles: [feat] ErrorMessage 추가

--- src/main/java/banking/constant/ErrorMessage.java
+++ src/main/java/banking/constant/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
   CHECK_GOAL_AMOUNT("목표 금액이 충족되지 않았습니다."),
   EXCLUDE_KOREAN("한글을 포함할 수 없습니다"),
   TYPE_FOR_DECIMAL("숫자로 입력해주세요."),
-  TYPE_FOR_ACCOUNT("계좌형식에 맞춰주세요.");
+  TYPE_FOR_ACCOUNT("계좌형식에 맞춰주세요."),
+  INCORRECT_INPUT("잘못된 입력입니다. 다시 입력해주세요.");
 
   private final String errorMessage;
 
